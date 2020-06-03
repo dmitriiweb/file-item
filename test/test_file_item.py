99081c62d1d6669902552052ba3ec7824be71f8c
Dmitrii Kurlov <dmitriik@tutanota.com>
2020-06-03 17:30:49 +0700

    tests refactoring

--- test/test_file_item.py
+++ test/test_file_item.py
@@ -6,6 +6,8 @@ from file_item import File
 
 class TestItem(unittest.TestCase):
     def setUp(self) -> None:
+        self.file_path = File('test.csv', '/home/user/test.csv')
+        self.file_no_path = File('test.csv')
         pass
 
     def test_basedir(self):
@@ -13,9 +15,7 @@ class TestItem(unittest.TestCase):
         self.assertEqual(basedir, Path(__file__).parent)
 
     def test_repr(self):
-        file = File('test.csv', '/home/user/test.csv')
-        self.assertEqual(repr(file), 'File(name="test.csv", path="/home/user/test.csv")')
+        self.assertEqual(repr(self.file_path), 'File(name="test.csv", path="/home/user/test.csv")')
 
     def test_repr_path_none(self):
-        file = File('test.csv')
-        self.assertEqual(repr(file), 'File(name="test.csv", path="./test.csv")')
+        self.assertEqual(repr(self.file_no_path), 'File(name="test.csv", path="./test.csv")')
